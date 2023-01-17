5237051fa2dd567e62a7d9c08ce831b87e32cd08
LincolnProenca <lincoln22220@gmail.com>
2023-01-17 04:02:22 -0300

    Melhorando responsividade do banner

--- fashion-zone/src/components/banner/index.js
+++ fashion-zone/src/components/banner/index.js
@@ -42,21 +42,29 @@ function Banner() {
           }}
         >
           <div className="banner1">
-            <img className="img1" src={img1} alt=""></img>
+            <div className="img1">
+              <img className="img1" src={img1} alt=""></img>
+            </div>
+
             <div className="Textos">
               <h1 className="text-head">Moda verão 40% off</h1>
               <button className="vermais">Ver mais</button>
             </div>
           </div>
           <div className="banner2">
-            <img className="img2" src={img2} alt=""></img>
+            <div className="img2">
+              <img className="img2" src={img2} alt=""></img>
+            </div>
+
             <div className="Textos">
               <h1 className="text-head">Populares</h1>
               <button className="vermais">Ver mais</button>
             </div>
           </div>
           <div className="banner3">
-            <img className="img3" src={img3} alt=""></img>
+            <div className="img3">
+              <img className="img3" src={img3} alt=""></img>
+            </div>
             <div className="Textos">
               <h1 className="text-head">Novidades</h1>
               <button className="vermais">Ver mais</button>
